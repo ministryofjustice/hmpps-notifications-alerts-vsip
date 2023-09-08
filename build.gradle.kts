0726b0ecbeebf95e1f674aa4b1be4325d282444a
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-09-08 09:53:54 +0100

    VB-2693 Updated of service with the latest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,53 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.3"
-  kotlin("plugin.spring") version "1.8.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0-beta"
+  kotlin("plugin.spring") version "1.9.0"
+  idea
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+repositories {
+  maven { url = uri("https://repo.spring.io/milestone") }
+  mavenCentral()
+}
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.28.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+  implementation("org.freemarker:freemarker:2.3.32")
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.16")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
+  testImplementation("org.testcontainers:localstack:1.18.3")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+  testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+  testImplementation("org.springframework.security:spring-security-test")
+  testImplementation("org.testcontainers:localstack:1.18.3")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.513")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "18"
+      jvmTarget = "19"
     }
   }
 }
