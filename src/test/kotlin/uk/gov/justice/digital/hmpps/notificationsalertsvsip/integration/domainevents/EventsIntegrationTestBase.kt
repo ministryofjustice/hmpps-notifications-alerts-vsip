a59517a23284f295c9259ec13b15cc828d3d56a3
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-11-16 16:16:56 +0000

    VB-2693 - updates to send SMS when visit booked, updated or cancelled.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/integration/domainevents/EventsIntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/integration/domainevents/EventsIntegrationTestBase.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.notificationsalertsvsip.integration.domainevents
 
-import com.fasterxml.jackson.core.json.JsonWriteFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.junit.jupiter.api.AfterAll
@@ -17,12 +16,17 @@ import org.springframework.test.context.DynamicPropertySource
 import software.amazon.awssdk.services.sns.model.PublishRequest
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
+import uk.gov.justice.digital.hmpps.notificationsalertsvsip.dto.visit.scheduler.ContactDto
+import uk.gov.justice.digital.hmpps.notificationsalertsvsip.dto.visit.scheduler.VisitDto
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.helper.JwtAuthHelper
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.integration.domainevents.LocalStackContainer.setLocalStackProperties
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.integration.mock.HmppsAuthExtension
+import uk.gov.justice.digital.hmpps.notificationsalertsvsip.integration.mock.PrisonRegisterMockServer
+import uk.gov.justice.digital.hmpps.notificationsalertsvsip.integration.mock.VisitSchedulerMockServer
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.DomainEventListenerService
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.NotificationService
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.PRISON_VISITS_NOTIFICATION_ALERTS_QUEUE_CONFIG_KEY
+import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.SmsSenderService
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.listeners.events.DomainEvent
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.listeners.events.EventFeatureSwitch
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.listeners.events.SQSMessage
@@ -32,6 +36,9 @@ import uk.gov.justice.digital.hmpps.notificationsalertsvsip.service.listeners.no
 import uk.gov.justice.hmpps.sqs.HmppsQueue
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.HmppsTopic
+import java.time.Duration
+import java.time.LocalDate
+import java.time.LocalTime
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -40,8 +47,9 @@ abstract class EventsIntegrationTestBase {
 
   companion object {
     private val localStackContainer = LocalStackContainer.instance
-    val objectMapper: ObjectMapper = ObjectMapper().registerModule(JavaTimeModule())
-      .disable(JsonWriteFeature.QUOTE_FIELD_NAMES.mappedFeature())
+    private val objectMapper: ObjectMapper = ObjectMapper().registerModule(JavaTimeModule())
+    val visitSchedulerMockServer = VisitSchedulerMockServer(objectMapper)
+    val prisonRegisterMockServer = PrisonRegisterMockServer(objectMapper)
 
     @JvmStatic
     @DynamicPropertySource
@@ -52,11 +60,15 @@ abstract class EventsIntegrationTestBase {
     @BeforeAll
     @JvmStatic
     fun startMocks() {
+      visitSchedulerMockServer.start()
+      prisonRegisterMockServer.start()
     }
 
     @AfterAll
     @JvmStatic
     fun stopMocks() {
+      visitSchedulerMockServer.stop()
+      prisonRegisterMockServer.stop()
     }
   }
 
@@ -68,6 +80,9 @@ abstract class EventsIntegrationTestBase {
 
   @SpyBean
   lateinit var notificationService: NotificationService
+
+  @SpyBean
+  lateinit var smsSenderService: SmsSenderService
 
   @SpyBean
   lateinit var prisonVisitBookedEventNotifierSpy: PrisonVisitBookedEventNotifier
@@ -149,5 +164,15 @@ abstract class EventsIntegrationTestBase {
     builder.append("\"reference\":\"$bookingReference\"")
     builder.append("}")
     return builder.toString()
+  }
+
+  fun createVisitDto(bookingReference: String, prisonCode: String = "HEI", visitDate: LocalDate, visitTime: LocalTime, duration: Duration, contactName: String, telephoneNumber: String): VisitDto {
+    return VisitDto(
+      reference = bookingReference,
+      prisonCode = prisonCode,
+      startTimestamp = visitDate.atTime(visitTime),
+      endTimestamp = visitDate.atTime(visitTime).plus(duration),
+      visitContact = ContactDto(contactName, telephoneNumber),
+    )
   }
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/integration/mock/VisitSchedulerMockServer.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.notificationsalertsvsip.integration.mock
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.notificationsalertsvsip.dto.visit.scheduler.VisitDto
+
+class VisitSchedulerMockServer(@Autowired private val objectMapper: ObjectMapper) : WireMockServer(8092) {
+  fun stubGetVisit(reference: String, visitDto: VisitDto?, httpStatus: HttpStatus = HttpStatus.NOT_FOUND) {
+    val responseBuilder = createJsonResponseBuilder()
+    stubFor(
+      get("/visits/$reference")
+        .willReturn(
+          if (visitDto == null) {
+            responseBuilder.withStatus(httpStatus.value())
+          } else {
+            responseBuilder.withStatus(HttpStatus.OK.value())
+              .withBody(getJsonString(visitDto))
+          },
+        ),
+    )
+  }
+
+  private fun getJsonString(obj: VisitDto): String {
+    return objectMapper.writer().writeValueAsString(obj)
+  }
+
+  private fun createJsonResponseBuilder(): ResponseDefinitionBuilder {
+    return aResponse().withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+  }
+}
