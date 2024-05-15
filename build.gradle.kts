27f488f7ccb2de00bb5723b5d195ec74b84b3d51
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-05-15 11:24:50 +0100

    Update dependencies and add dependencyCheck as dependency-check-suppress file was being ignored.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
   kotlin("plugin.spring") version "1.9.23"
   id("org.jetbrains.kotlin.plugin.noarg") version "1.9.23"
 }
@@ -13,21 +13,25 @@ repositories {
   mavenCentral()
 }
 
+dependencyCheck {
+  suppressionFiles.add("dependency-check-suppress-json.xml")
+}
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-cache")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.3")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.2.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.3.0")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
-  implementation("org.springframework.data:spring-data-commons:3.2.4")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1")
+  implementation("org.springframework.data:spring-data-commons:3.2.5")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.5.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
   implementation("org.springdoc:springdoc-openapi-starter-common:2.5.0")
@@ -35,10 +39,10 @@ dependencies {
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.21")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.19.7")
+  testImplementation("org.testcontainers:localstack:1.19.8")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
