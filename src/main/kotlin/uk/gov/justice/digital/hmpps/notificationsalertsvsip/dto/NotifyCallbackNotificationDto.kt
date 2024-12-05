84915f487a7d5c2361b34384f97234d694b782aa
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-12-05 09:23:46 +0000

    temp - convert all to nullable for debugging

--- src/main/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/dto/NotifyCallbackNotificationDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/dto/NotifyCallbackNotificationDto.kt
@@ -16,13 +16,13 @@ data class NotifyCallbackNotificationDto(
   @Schema(description = "The final status of the notification", required = true)
   val status: String,
   @Schema(description = "The timestamp for when the vsip notification service sent the notification to gov notify", required = true)
-  val createdAt: String,
+  val createdAt: String?,
   @Schema(description = "The timestamp for the final update of the notification (when delivered or ultimately failed) ", required = false)
   val completedAt: String?,
   @Schema(description = "The timestamp for when gov notify sent the notification", required = false)
   val sentAt: String?,
   @Schema(description = "The type of the notification", required = true)
-  val notificationType: String,
+  val notificationType: String?,
   @Schema(description = "The id the template used for the notification", required = true)
   val templateId: UUID,
   @Schema(description = "The version of the template used for the notification", required = true)
