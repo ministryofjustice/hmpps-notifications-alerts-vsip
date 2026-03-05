da159b4ce37bdecdccb2d052fd0d279c15963ea5
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-03-05 12:45:38 +0000

    Upgrade sqs lib to v7

--- src/main/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/service/listeners/events/SQSMessage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/service/listeners/events/SQSMessage.kt
@@ -6,11 +6,25 @@ import jakarta.validation.constraints.NotBlank
 @Suppress("PropertyName")
 data class SQSMessage(
   @field:NotBlank
-  @param:JsonProperty("Type")
-  val type: String,
-  @field:NotBlank
   @param:JsonProperty("Message")
   val message: String,
+
   @param:JsonProperty("MessageId")
   val messageId: String? = null,
+
+  @param:JsonProperty("MessageAttributes")
+  val messageAttributes: MessageAttributes = MessageAttributes(),
 )
+
+data class MessageAttribute(
+  @param:JsonProperty("Type")
+  val type: String? = null,
+
+  @param:JsonProperty("Value")
+  val value: String? = null,
+)
+
+class MessageAttributes : HashMap<String, MessageAttribute>() {
+  val eventType: String?
+    get() = this["eventType"]?.value
+}
