00b8236267b6355dc80cc62dcd85c4f7864f782c
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-08-03 10:01:25 +0100

    dep updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.2"
   kotlin("plugin.spring") version "2.4.10"
   id("org.jetbrains.kotlin.plugin.noarg") version "2.4.10"
   id("org.owasp.dependencycheck") version "12.2.2"
@@ -24,7 +24,7 @@ dependencies {
 
   implementation("org.springframework.data:spring-data-commons:4.1.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
-  implementation("uk.gov.service.notify:notifications-java-client:6.0.1-RELEASE")
+  implementation("uk.gov.service.notify:notifications-java-client:6.1.0-RELEASE")
   implementation("org.springframework.boot:spring-boot-configuration-processor")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:3.0.0")
