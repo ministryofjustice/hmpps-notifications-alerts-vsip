ccf229784fbdd0534ae8b2714e5d03b8798333ce
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-08-07 10:52:04 +0100

    VB-6594 - Welsh notifications
    SMS - Visit changed notification in welsh

--- src/main/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/service/handlers/sms/UpdatedEventVisitsSmsHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/notificationsalertsvsip/service/handlers/sms/UpdatedEventVisitsSmsHandler.kt
@@ -9,6 +9,7 @@ import uk.gov.justice.digital.hmpps.notificationsalertsvsip.enums.SmsTemplateNam
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.utils.DateUtils.Companion.getFormattedDate
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.utils.DateUtils.Companion.getFormattedDayOfWeek
 import uk.gov.justice.digital.hmpps.notificationsalertsvsip.utils.DateUtils.Companion.getFormattedTime
+import java.util.Locale
 
 @Service
 class UpdatedEventVisitsSmsHandler : BaseVisitsSmsNotificationHandler() {
@@ -24,16 +25,27 @@ class UpdatedEventVisitsSmsHandler : BaseVisitsSmsNotificationHandler() {
   }
 
   private fun getTemplateVars(visit: VisitDto): Map<String, String> {
+    val prison = prisonRegisterService.getPrison(visit.prisonCode)
+
     val templateVars = mutableMapOf(
       "ref number" to visit.reference,
       "servicename" to serviceName,
-      "prison" to (prisonRegisterService.getPrison(visit.prisonCode)?.prisonName ?: visit.prisonCode),
+      "prison" to (prison?.prisonName ?: visit.prisonCode),
       "time" to getFormattedTime(visit.startTimestamp.toLocalTime()),
       "dayofweek" to getFormattedDayOfWeek(visit.startTimestamp.toLocalDate()),
       "date" to getFormattedDate(visit.startTimestamp.toLocalDate()),
     )
+
     when (visit.visitContact.languagePreference) {
-      LanguagePreference.CY -> templateVars.putAll(emptyMap<String, String>())
+      LanguagePreference.CY -> templateVars.putAll(
+        mapOf(
+          "prison_cy" to (prison?.prisonNameInWelsh ?: prison?.prisonName ?: visit.prisonCode),
+          "servicename_cy" to welshServiceName,
+          "dayofweek_cy" to getFormattedDayOfWeek(visit.startTimestamp.toLocalDate(), Locale.forLanguageTag("cy-GB")),
+          "date_cy" to getFormattedDate(visit.startTimestamp.toLocalDate(), Locale.forLanguageTag("cy-GB")),
+        ),
+      )
+
       else -> Unit
     }
 
